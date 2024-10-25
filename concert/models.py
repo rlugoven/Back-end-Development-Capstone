c47a26363426440a8766efdeea66c9ae27969377
Raquel Lugo-Vecchini <165518342+rlugoven@users.noreply.github.com>
2024-10-25 08:56:49 -0400

    Update models.py

--- concert/models.py
+++ concert/models.py
@@ -8,10 +8,10 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 class Concert(models.Model):
-    # concert_name
-    # duration
-    # city
-    # date
+    concert_name = models.CharField(max_length=255)
+    duration = models.IntegerField()
+    city = models.CharField(max_length=255)
+    date = models.DateField(default=datetime.now)
 
     def __str__(self):
         return self.concert_name
@@ -37,6 +37,34 @@ class ConcertAttending(models.Model):
         unique_together = ['concert', 'user']
 
     def __str__(self):
+        return self.attending
+
+
+class Photo(models.Model):
+    id = models.IntegerField(primary_key=True)
+    pic_url = models.CharField(max_length=1000)
+    event_country = models.CharField(max_length=255)
+    event_state = models.CharField(max_length=255)
+    event_city = models.CharField(max_length=255)
+    event_date = models.DateField(default=datetime.now)
+
+    class Meta:
+        managed = False
+
+    def __str__(self):
+        return self.pic_url
+
+
+class Song(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    lyrics = models.TextField()
+
+    class Meta:
+        managed = False
+
+    def __str__(self):
+        return self.title
         return self.attending
 
 
